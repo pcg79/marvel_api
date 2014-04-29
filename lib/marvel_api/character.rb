a33dcaea3c633b8e1ffbdc7f98d0ce4123be0ee8
Pat George <pat.george@hungrymachine.com>
2014-04-29 13:18:24 -0400

    Added top level attributes to Character

--- lib/marvel_api/character.rb
+++ lib/marvel_api/character.rb
@@ -3,6 +3,18 @@ require 'marvel_api/api_model'
 module MarvelApi
   class Character < ApiModel
 
+    attribute :id
+    attribute :name
+    attribute :description
+    attribute :modified
+    attribute :thumbnail
+    attribute :resourceURI
+    attribute :comics
+    attribute :series
+    attribute :stories
+    attribute :events
+    attribute :urls
+
     def endpoint
       "characters"
     end
